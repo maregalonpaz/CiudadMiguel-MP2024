ffd981d1cc8c0d1281934345937f9ad739bf504f
Mangel <maregalonpaz@gmail.com>
2024-02-12 18:32:49 +0100

    Primer Push

---
+++ function.js
@@ -0,0 +1,24 @@
+
+function responder(esCorrecta) {
+    if (esCorrecta) {
+        document.getElementById("respuesta").innerText = "¡Respuesta correcta!";
+    } else {
+        document.getElementById("respuesta").innerText = "Respuesta incorrecta. Inténtalo de nuevo.";
+    }
+}
+
+const slider = document.getElementById('slider');
+        const slides = document.querySelectorAll('.slide');
+        const slideWidth = slides[0].clientWidth;
+        let currentSlide = 0;
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % slides.length;
+            updateSlider();
+        }
+
+        function updateSlider() {
+            slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+        }
+
+        setInterval(nextSlide, 3500);
