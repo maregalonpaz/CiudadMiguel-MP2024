cc62c0f9fd9721ab88e2a5227b8aac372ce8c0a8
Mangel <maregalonpaz@gmail.com>
2024-02-12 19:29:39 +0100

    third push

--- function.js
+++ function.js
@@ -7,6 +7,14 @@ function responder(esCorrecta) {
     }
 }
 
+function responder(esCorrecta) {
+    if (esCorrecta) {
+        document.getElementById("respuesta2").innerText = "¡Respuesta correcta!";
+    } else {
+        document.getElementById("respuesta2").innerText = "Respuesta incorrecta. Inténtalo de nuevo.";
+    }
+}
+
 const slider = document.getElementById('slider');
         const slides = document.querySelectorAll('.slide');
         const slideWidth = slides[0].clientWidth;
